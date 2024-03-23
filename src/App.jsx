0895ed6c64119e302723806b3506006ddf358de3
AlfinAfifan <agusalfin1@gmail.com>
2024-03-23 15:21:45 +0700

    update image & cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Bars3BottomRightIcon, EnvelopeIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import { useEffect, useState } from 'react';
-import Profil from './assets/images/profil.jpg';
+import Profil from './assets/images/foto.jpg';
 import data from './data.json';
 
 function App() {
@@ -98,7 +98,7 @@ function App() {
             I am a person who loves the challenges and the world of technology. I am committed to continuously learning and keeping up with the developments in programming languages. The reason I love programming is because of the
             challenges inherent in the programming world, especially when encountering errors during the application development process.
           </p>
-          <a href="https://drive.google.com/file/d/1qgPUDfKfUOP0evC_Ue9cQeX-AN4wSrei/view?usp=sharing" className="bg-sky-700 mt-8 px-5 text-sm lg:text-base hover:bg-sky-800 py-2 rounded-full text-white">
+          <a href="https://drive.google.com/file/d/1BY5Fg9t47yNPweu3NK87TRoaP48Jn6wT/view?usp=sharing" target="_blank" className="bg-sky-700 mt-8 px-5 text-sm lg:text-base hover:bg-sky-800 py-2 rounded-full text-white">
             Curriculum Vitae
           </a>
         </div>
@@ -112,14 +112,14 @@ function App() {
           <div className="text-sm lg:text-base mx-auto">About Me</div>
           <h1 className="text-xl lg:text-2xl font-semibold mx-auto text-sky-800">Let Me Introduce Myself</h1>
           <p className="text-sm lg:text-base text-justify mx-auto mt-5">My full name is Agus Alfin 'Afifan Mahfudh, commonly known as Alfin. At my age that's 22 years old I have some experience, including :</p>
-          <div className="flex items-center gap-7 mt-5 lg:mt-10 justify-start">
-            <h1 className="font-semibold w-[30%] ">Graphic Designer Instructor</h1>
-            <p className="text-justify w-[70%]">As a freelance graphic designer and graphic design instructor at one of the training institutions called LKP Andalas.</p>
+          <div className="flex items-center gap-7 mt-5 justify-start">
+            <h1 className="font-semibold w-[30%] ">Front End Web Developer</h1>
+            <p className="text-justify w-[70%]">Internship program at Sekawan Media company as a frontend web developer using React Typescript.</p>
           </div>
           <div className="h-0.5 w-full bg-slate-300 my-6"></div>
-          <div className="flex items-center justify-start gap-7">
-            <h1 className="font-semibold w-[30%]">Web Developer</h1>
-            <p className="text-justify w-[70%]">Bootcamp as fullstack web developer during 4 months in PT. Arkatama Multi Solusindo using Laravel and MySql.</p>
+          <div className="flex items-center gap-7 mt-5 justify-start">
+            <h1 className="font-semibold w-[30%] ">Web Developer</h1>
+            <p className="text-justify w-[70%]">As a freelance web developer with various projects that have been completed.</p>
           </div>
           <div className="h-0.5 w-full bg-slate-300 my-6"></div>
           <div className="flex items-center gap-7 justify-start">
@@ -127,9 +127,14 @@ function App() {
             <p className="text-justify w-[70%]">Internship program at BAKORWIL 3 Malang instance as a fullstack web developer using React Js, Express Js and MySql.</p>
           </div>
           <div className="h-0.5 w-full bg-slate-300 my-6"></div>
-          <div className="flex items-center gap-7 justify-start">
-            <h1 className="font-semibold w-[30%] ">Web Developer</h1>
-            <p className="text-justify w-[70%]">As a freelance web developer with various projects that have been completed.</p>
+          <div className="flex items-center justify-start gap-7">
+            <h1 className="font-semibold w-[30%]">Web Developer</h1>
+            <p className="text-justify w-[70%]">Bootcamp as fullstack web developer during 4 months in PT. Arkatama Multi Solusindo using Laravel and MySql.</p>
+          </div>
+          <div className="h-0.5 w-full bg-slate-300 my-6"></div>
+          <div className="flex items-center gap-7 lg:mt-10 justify-start">
+            <h1 className="font-semibold w-[30%] ">Graphic Designer Instructor</h1>
+            <p className="text-justify w-[70%]">As a freelance graphic designer and graphic design instructor at one of the training institutions called LKP Andalas.</p>
           </div>
         </div>
       </div>
